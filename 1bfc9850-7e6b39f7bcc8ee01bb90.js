235d8b56fd2eb31b398dfb1e88f7e83df229403e
Rahul T <rahulmanojcoder@gmail.com>
2022-02-01 19:52:28 +0530

    Updates

---
+++ 1bfc9850-7e6b39f7bcc8ee01bb90.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgatsby_starter_hello_world=self.webpackChunkgatsby_starter_hello_world||[]).push([[445],{3201:function(t,r,l){l.d(r,{$Pg:function(){return c}});var a=l(1046);function c(t){return(0,a.w_)({tag:"svg",attr:{viewBox:"0 0 320 512"},child:[{tag:"path",attr:{d:"M177 159.7l136 136c9.4 9.4 9.4 24.6 0 33.9l-22.6 22.6c-9.4 9.4-24.6 9.4-33.9 0L160 255.9l-96.4 96.4c-9.4 9.4-24.6 9.4-33.9 0L7 329.7c-9.4-9.4-9.4-24.6 0-33.9l136-136c9.4-9.5 24.6-9.5 34-.1z"}}]})(t)}}}]);
+//# sourceMappingURL=1bfc9850-7e6b39f7bcc8ee01bb90.js.map
