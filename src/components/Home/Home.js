ce3f998f24b4dbef234dd38d9e6417d630750164
Rahul T <rahulmanojcoder@gmail.com>
2022-02-01 14:41:59 +0530

    slider on home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import "./Home.scss"
 import home from "../../assets/home.jpg"
+
+import { Carousel } from 'react-responsive-carousel';
+import "react-responsive-carousel/lib/styles/carousel.min.css"; 
+
+
 const Home = () => {
   return <section className='section Home'>
     <div className="content">
@@ -8,9 +13,21 @@ const Home = () => {
         <p className="desc">One stop shop for all your engineering solutions</p>
         <button className="btn">Get started</button>
     </div>
-    <div className="image">
-        <img src={home} alt="home" />
-    </div>
+    <Carousel className='images'
+            autoPlay={true}
+            showIndicators={false}
+            showArrows={false}
+            showThumbs={false}
+            infiniteLoop={true}
+        >
+        <div className="image">
+            <img src={home} alt="" />
+        </div>
+        <div className="image">
+            <img src={home} alt="" />
+        </div>
+       
+        </Carousel>
   </section>;
 };
 
