f66d0bb12ca95666ff9b352ce72a2404e63b6876
Rahul T <rahulmanojcoder@gmail.com>
2022-02-01 21:45:53 +0530

    og image

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,11 +41,11 @@ export default function Home() {
     <section>
      <Helmet title="New Point Electromechanical">
       <meta name="description" content="Destination for all your engineering needs, One stop shop for all engineering solutions | Newpoint Electromechanical Equipment Installation and Maintenance" />
-      <meta name="image" content="https://github.com/elaventa/newpointuae/blob/577f20ffcd334cf127a94a44f0379a80ef5fe8f6/src/assets/og-image.png" />
+      <meta name="image" content="https://github.com/elaventa/newpointuae/blob/577f20ffcd334cf127a94a44f0379a80ef5fe8f6/src/assets/og-image.png?raw=true" />
       <meta property="og:site_name" content="Newpoint Electromechanical Equipment Installation and Maintenance " />
       <meta property="og:title" content="Newpoint Electromechanical Equipment Installation and Maintenance" />
       <meta property="og:url" content="https://newpointuae.com" />
-      <meta property="og:image" itemprop="image" content="https://github.com/elaventa/newpointuae/blob/577f20ffcd334cf127a94a44f0379a80ef5fe8f6/src/assets/og-image.png" />
+      <meta property="og:image" itemprop="image" content="https://github.com/elaventa/newpointuae/blob/577f20ffcd334cf127a94a44f0379a80ef5fe8f6/src/assets/og-image.png?raw=true" />
       <meta property="og:type" content="website" />
       <meta property="og:description" content="Destination for all your engineering needs, One stop shop for all engineering solutions | Newpoint Electromechanical Equipment Installation and Maintenance" />
     </Helmet>
